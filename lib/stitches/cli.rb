53eab0f12c82d8ca5fdd185f092798e40d038c4b
t3rro <t3rro@protonmail.com>
2023-02-23 05:51:50 -0800

    add src

--- lib/stitches/cli.rb
+++ lib/stitches/cli.rb
@@ -6,35 +6,9 @@ class Command
   include TTY::Option
 
   usage do
+    desc %(manage stitches configuration)
     program %(stitches)
-
-    command %(run)
-
-    desc %(Run a command in a new container)
-
-    example %(Set working directory (-w)),
-            %(  $ dock run -w /path/to/dir/ ubuntu pwd)
-
-    example <<~MOUNT
-      Mount volume
-        $ dock run -v `pwd`:`pwd` -w `pwd` ubuntu pwd
-    MOUNT
-  end
-
-  argument :image do
-    required
-    desc %(The name of the image to use)
-  end
-
-  argument :command do
-    optional
-    desc %(The command to run inside the image)
-  end
-
-  keyword :restart do
-    default %(no)
-    permit %w[no on-failure always unless-stopped]
-    desc %(Restart policy to apply when a container exits)
+    command %(config)
   end
 
   flag :help do
@@ -43,27 +17,8 @@ class Command
     desc %(Print usage)
   end
 
-  flag :detach do
-    short %(-d)
-    long %(--detach)
-    desc %(Run container in background and print container ID)
-  end
-
-  option :name do
-    required
-    long %(--name string)
-    desc %(Assign a name to the container)
-  end
-
-  option :port do
-    arity one_or_more
-    short %(-p)
-    long %(--publish list)
-    convert :list
-    desc %(Publish a container's port(s) to the host)
-  end
-
   def run
+    parse(ARGV)
     if params[:help]
       print help
       exit
