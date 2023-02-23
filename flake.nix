2c0de6ca4b462d4668cf166333a8cbb648c15f43
t3rro <t3rro@protonmail.com>
2023-02-23 13:44:38 -0800

    add src

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
   inputs.nixpkgs.url = github:NixOS/nixpkgs;
   inputs.ruby-flake-utils.url = github:t3rro/ruby-flake-utils;
 
-  outputs = { ruby-flake-utils, nixpkgs, ... }: ruby-flake-utils.lib.mkGemSystems {
-    inherit nixpkgs;
-    name = "stitches-cli";
-    lockfile = ./Gemfile.lock;
-    gemfile = ./Gemfile;
-    gemset = ./gemset.nix;
-    strategy = "bin";
-    src = ./.;
-  };
+  outputs = { ruby-flake-utils, nixpkgs, ... }:
+    ruby-flake-utils.lib.mkGemSystems {
+      inherit nixpkgs;
+      name = "stitches-cli";
+      lockfile = ./Gemfile.lock;
+      gemfile = ./Gemfile;
+      gemset = ./gemset.nix;
+      strategy = "bin";
+      src = ./.;
+    };
 }
