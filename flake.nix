5ca1984e68eca5e724b7ac55fd6856e125b8c273
t3rro <t3rro@protonmail.com>
2023-02-23 01:40:12 -0800

    init

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "stitches-cli";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
+  inputs.ruby-flake-utils.url = github:t3rro/ruby-flake-utils;
+
+  outputs = { ruby-flake-utils, nixpkgs, ... }:
+    ruby-flake-utils.lib.mkGemSystems
+      "stitches-cli"
+      nixpkgs
+      ./Gemfile.lock
+      ./Gemfile
+      ./gemset.nix
+  ;
+}
