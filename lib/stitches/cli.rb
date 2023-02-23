5ee9c2141f7dbca860353b35743c9172bbdb3782
t3rro <t3rro@protonmail.com>
2023-02-23 12:36:38 -0800

    add src

--- lib/stitches/cli.rb
+++ lib/stitches/cli.rb
@@ -1,18 +1,22 @@
 require %(tty-option)
 
-class ConfigCommand
+# shared command behavior for all commands in Stitches
+class StitchesCommand
+  include TTY::Option
+  flag :help do
+    short %(-h)
+    long %(--help)
+    desc %(Print usage)
+  end
+end
+
+class ConfigCommand < StitchesCommand
   include TTY::Option
 
   usage do
     desc %(manage configuration)
     program %(stitches)
     command %(config)
-  end
-
-  flag :help do
-    short %(-h)
-    long %(--help)
-    desc %(Print usage)
   end
 
   def run(argv)
@@ -26,18 +30,33 @@ class ConfigCommand
   end
 end
 
-class Command
-  include TTY::Option
+class InfraCommand < StitchesCommand
+  usage do
+    desc %(manage configuration)
+    program %(stitches)
+    command %(infa)
+  end
 
+  def run(argv)
+    parse(argv)
+    if params[:help]
+      print help
+      exit
+    else
+      pp params.to_h
+    end
+  end
+end
+
+class Command < StitchesCommand
   usage do
     desc %(stitch together infrastructure)
     program %(stitches)
   end
 
-  flag :help do
-    short %(-h)
-    long %(--help)
-    desc %(Print usage)
+  argument :subcommand do
+    desc %(subcommand for stitches)
+    required
   end
 
   def run
@@ -48,7 +67,10 @@ class Command
       print help
       exit
     else
-      [ConfigCommand.new].each do |scmd|
+      [
+        ConfigCommand.new,
+        InfraCommand.new
+      ].each do |scmd|
         scmd.run(argv)
       end
     end
