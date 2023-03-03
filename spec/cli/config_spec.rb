d7e5d492a58bb530f6bc4a3736553d18b6ad778a
t3rro <t3rro@protonmail.com>
2023-03-03 08:56:15 -0800

    add toml-rb

--- spec/cli/config_spec.rb
+++ spec/cli/config_spec.rb
@@ -5,6 +5,12 @@ require %(stitches/cli/config)
 describe Config do
   context %(#resolve_configuration) do
     defaults = { ignore_default_paths: true }
-    expect(Config.resolve_configurations(**defaults.merge({ extra_paths: [] }))).to be_empty
+    it %(empty when paths ignored) do
+      expect(
+        Config.resolve_configurations(
+          **defaults.merge({ extra_paths: [] })
+        )
+      ).to be_empty
+    end
   end
 end
